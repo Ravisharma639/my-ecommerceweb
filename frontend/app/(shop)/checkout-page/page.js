3d5153a71579df2bfd7657f0ca0edafa345fc2e7
ravisharma639 <ravi82639f@gmail.com>
2025-11-13 02:15:24 +0530

    Fix CartContext import path for Vercel

--- frontend/app/(shop)/checkout-page/page.js
+++ frontend/app/(shop)/checkout-page/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { useCart } from "../../context/CartContext";
+import { useCart } from "../../../context/CartContext";
 
 export default function CheckoutPage() {
   const router = useRouter();
